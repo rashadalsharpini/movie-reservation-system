efe629aa3812a6e7c9cdf25ab8a0c992d24ac091
Adel Mohamed <adel.erorr159@gmail.com>
2025-09-24 16:00:44 +0300

    FinishingMovieController

--- Presentation/Controller/MovieController.cs
+++ Presentation/Controller/MovieController.cs
@@ -20,5 +20,23 @@ public class MovieController(IServiceManager serviceManager) : ApiBaseController
         var result = await serviceManager.MovieService.GetByIdAsync(id);
         return Ok(result);
     }
+
+    public async Task<ActionResult<ResponseMovieScheduleDto>> CreateAsync(CreateMovieDto dto)
+    {
+        var result = await serviceManager.MovieService.CreateAsync(dto);
+        return Ok(result);
+    }
+
+    public async Task<ActionResult<bool>> UpdateAsync(Guid id, UpdateMovieDto dto)
+    {
+        var result = await serviceManager.MovieService.UpdateAsync(id, dto);
+        return Ok(result);
+    }
+
+    public async Task<ActionResult<bool>> DeleteAsync(Guid id)
+    {
+        var result = await serviceManager.MovieService.DeleteAsync(id);
+        return Ok(result);
+    }
     
 }
