590b6303704ffda48685ff1649f41726574b383f
rashad <rashadalsharpini@gmail.com>
2025-09-24 15:35:26 +0300

    getAll-getByIdMovie

--- Presentation/Controller/MovieController.cs
+++ Presentation/Controller/MovieController.cs
@@ -1,6 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using Shared;
+using Shared.Dtos;
+
 namespace Presentation.Controller;
 
-public class MovieController:ApiBaseController
+public class MovieController(IServiceManager serviceManager) : ApiBaseController
 {
+    [HttpGet]
+    public async Task<ActionResult<PaginatedResult<ResponseMovieScheduleDto>>> GetAllMovieSchedules(
+        [FromQuery] MovieParameterSpecification parameters)
+    {
+        var result = await serviceManager.MovieService.GetAllAsync(parameters);
+        return Ok(result);
+    }
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<ResponseMovieScheduleDto>> GetMovieById(Guid id)
+    {
+        var result = await serviceManager.MovieService.GetByIdAsync(id);
+        return Ok(result);
+    }
     
 }
