5d17b4651836bb49060de6c5fde4472f4560ddb7
Adel Mohamed <adel.erorr159@gmail.com>
2025-10-05 23:55:40 +0300

    GetAllSeatMethod

--- Service/SeatService.cs
+++ Service/SeatService.cs
@@ -1,16 +1,21 @@
 using AutoMapper;
 using Domain.Contracts;
 using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using ServiceAbstraction;
 using Shared.Dtos;
 
 namespace Service;
 
-public class SeatService(IUnitOfWork unitOfWork, IMapper mapper):ISeatService
+public class SeatService(IUnitOfWork unitOfWork):ISeatService
 {
-    public Task<IEnumerable<ResponseSeatDto>> GetAllSeatsAsync(int scheduleId)
+    public async Task<IEnumerable<ResponseSeatDto>> GetAllSeatsAsync(int scheduleId)
     {
-        throw new NotImplementedException();
+        var schedule = await unitOfWork.GetRepo<Schedule, int>().Queryable()
+            .Include(s => s.Hall)
+            .ThenInclude(h => h.Seats).FirstOrDefaultAsync(s=>s.Id == scheduleId);
+       
+
     }
 
     public Task<bool> AreSeatsAvailableAsync(int scheduleId, List<int> seatIds)
--- Shared/Dtos/ResponseSeatDto.cs
+++ Shared/Dtos/ResponseSeatDto.cs
@@ -2,7 +2,8 @@ namespace Shared.Dtos;
 
 public class ResponseSeatDto
 {
+    public int SeatId { get; set; } 
     public int SeatNumber { get; set; }
     public int SeatRow { get; set; }
-    public bool IsAvailable { get; set; } = true;
+    public string Status { get; set; } = null!;
 }
