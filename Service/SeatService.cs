1b48c7f718376cbbba84ff596618e7709b91557e
Adel Mohamed <adel.erorr159@gmail.com>
2025-10-06 00:21:42 +0300

    FinishingGetAllSeats

--- Service/SeatService.cs
+++ Service/SeatService.cs
@@ -7,15 +7,29 @@ using Shared.Dtos;
 
 namespace Service;
 
-public class SeatService(IUnitOfWork unitOfWork):ISeatService
+public class SeatService(IUnitOfWork unitOfWork) : ISeatService
 {
     public async Task<IEnumerable<ResponseSeatDto>> GetAllSeatsAsync(int scheduleId)
     {
         var schedule = await unitOfWork.GetRepo<Schedule, int>().Queryable()
             .Include(s => s.Hall)
-            .ThenInclude(h => h.Seats).FirstOrDefaultAsync(s=>s.Id == scheduleId);
-       
+            .ThenInclude(h => h.Seats).FirstOrDefaultAsync(s => s.Id == scheduleId);
+        var bookedSeats = await unitOfWork.GetRepo<Ticket, Guid>().Queryable()
+            .Where(t => t.ScheduleId == scheduleId)
+            .Select(t => t.SeatId).ToListAsync();
+        var reservedSeats = await unitOfWork.GetRepo<SeatReservation, Guid>().Queryable()
+            .Where(sr => sr.ScheduleId == scheduleId)
+            .Select(sr => sr.SeatId).ToListAsync();
+        var result = schedule.Hall.Seats.Select(s => new ResponseSeatDto()
+        {
+            SeatId = s.Id,
+            SeatNumber = s.SeatNumber,
+            SeatRow = s.SeatRow,
+            Status = bookedSeats.Contains(s.Id) ? "Booked" :
+                reservedSeats.Contains(s.Id) ? "Reserved" : "Available"
+        }).ToList();
 
+        return result;
     }
 
     public Task<bool> AreSeatsAvailableAsync(int scheduleId, List<int> seatIds)
